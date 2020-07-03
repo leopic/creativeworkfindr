0b29305e60b2eb3b0e9839ade91e8d099c34d335
Leo Picado <leopic@gmail.com>
2020-07-02 20:22:23 -0600

    Allow cell to self-resize to match summary height

--- CreativeWorkFindr/Views/CreativeWorkCell.swift
+++ CreativeWorkFindr/Views/CreativeWorkCell.swift
@@ -9,21 +9,12 @@ class CreativeWorkCell: UITableViewCell {
 
   var work: CreativeWork! {
     didSet {
-      DispatchQueue.main.async { [weak self] in
-        guard let self = self else { return }
+      self.titleLabel.text = self.work.title
+      self.yearLabel.text = self.work.year
+      self.summaryLabel.text = "\(self.work is Movie ? "Plot:" : "Summary:") \(self.work.summary)"
 
-        self.titleLabel.text = self.work.title
-        self.yearLabel.text = self.work.year
-        self.summaryLabel.text = "\(self.work is Movie ? "Plot:" : "Summary:") \(self.work.summary)"
-
-        guard let thumbnailURL = self.work.thumbnailURL else { return }
-        self.thumbnail.sd_setImage(with: thumbnailURL, placeholderImage: UIImage(named: "no-image"))
-      }
+      guard let thumbnailURL = self.work.thumbnailURL else { return }
+      self.thumbnail.sd_setImage(with: thumbnailURL, placeholderImage: UIImage(named: "no-image"))
     }
-  }
-
-  override func prepareForReuse() {
-    super.prepareForReuse()
-    thumbnail.image = UIImage(named: "no-image")
   }
 }
